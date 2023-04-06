21fc7015b4f2fb95f2492a2ca3439db53cee36cf
Jeroen de Bruijn <vv9239@gmail.com>
2023-04-06 21:13:11 +0200

    feat: add package version to Rollbar errors

--- src/rollbar.ts
+++ src/rollbar.ts
@@ -1,10 +1,12 @@
 import Rollbar from 'rollbar';
+import {readPackageSync} from 'read-pkg';
 import {config} from './config.js';
 
 const rollbar = new Rollbar({
   accessToken: config.rollbarToken,
   captureUncaught: true,
   captureUnhandledRejections: true,
+  codeVersion: readPackageSync().version,
 });
 
 export default rollbar;
